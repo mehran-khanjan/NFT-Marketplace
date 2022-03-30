2df9ed0a4d59e5257786076a18777e1ab7306cfe
unknown <mehrankh1913@gmail.com>
2022-03-30 09:45:43 +0430

    Add some modifiers

--- Marketplace.sol
+++ Marketplace.sol
@@ -9,4 +9,15 @@ contract ArtMarketplace {
     constructor(NFT _token) {
         token = _token;
     }
+
+    modifier OnlyItemOwner(uint256 tokenId){
+        require(token.ownerOf(tokenId) == msg.sender, "Sender does not own the item");
+        _;
+    }
+
+    modifier HasTransferApproval(uint256 tokenId){
+        require(token.getApproved(tokenId) == address(this), "Market is not approved");
+        _;
+    }
+
 }
