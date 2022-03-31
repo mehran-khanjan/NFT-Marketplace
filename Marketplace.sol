3238072c98b68a0f40ec29f6078d59a77b72e754
unknown <mehrankh1913@gmail.com>
2022-03-31 09:03:06 +0430

    Add more modifiers

--- Marketplace.sol
+++ Marketplace.sol
@@ -6,6 +6,20 @@ import "./NFT.sol";
 contract ArtMarketplace {
     NFT private token;
 
+    struct ItemForSale {
+        uint256 id;
+        uint256 tokenId;
+        address payable seller;
+        uint256 price;
+        bool isSold;
+    }
+
+    ItemForSale[] public itemsForSale;
+    mapping(uint256 => bool) public activeItems; // tokenId => ativo?
+
+    event itemAddedForSale(uint256 id, uint256 tokenId, uint256 price);
+    event itemSold(uint256 id, address buyer, uint256 price);
+
     constructor(NFT _token) {
         token = _token;
     }
@@ -17,6 +31,16 @@ contract ArtMarketplace {
 
     modifier HasTransferApproval(uint256 tokenId){
         require(token.getApproved(tokenId) == address(this), "Market is not approved");
+        _;
+    }
+
+    modifier ItemExists(uint256 id){
+        require(id < itemsForSale.length && itemsForSale[id].id == id, "Could not find item");
+        _;
+    }
+
+    modifier IsForSale(uint256 id){
+        require(!itemsForSale[id].isSold, "Item is already sold");
         _;
     }
 
