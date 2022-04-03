b296a537ec1139fb46d97af8ed84c4cb5573bdbd
unknown <mehrankh1913@gmail.com>
2022-04-03 09:06:13 +0430

    Add buy item function

--- Marketplace.sol
+++ Marketplace.sol
@@ -66,4 +66,21 @@ contract ArtMarketplace {
         return newItemId;
     }
 
+    function buyItem(uint256 id)
+    ItemExists(id)
+    IsForSale(id)
+    HasTransferApproval(itemsForSale[id].tokenId)
+    payable
+    external {
+        require(msg.value >= itemsForSale[id].price, "Not enough funds sent");
+        require(msg.sender != itemsForSale[id].seller);
+
+        itemsForSale[id].isSold = true;
+        activeItems[itemsForSale[id].tokenId] = false;
+        token.safeTransferFrom(itemsForSale[id].seller, msg.sender, itemsForSale[id].tokenId);
+        itemsForSale[id].seller.transfer(msg.value);
+
+        emit itemSold(id, msg.sender, itemsForSale[id].price);
+    }
+
 }
